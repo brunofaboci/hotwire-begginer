92ed36532b6d8d081405e7891d524da0521aa9c4
Bruno Faboci <brunofaboci@gmail.com>
2022-09-06 19:50:25 -0300

    created broadcast for action create

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,17 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit -> { broadcast_prepend_to 'quotes', partial: 'quotes/quote', locals: { quote: self }, target: 'quotes' }
+
+  # por padrao, a opcao "target" é igual a "model_name.plural", que, nesse caso, é igual a "quotes",
+  # então podemos remover o parametro "target"
+
+  # o valor padrão da opção "partial" é o mesmo que chamar "to_partial_path em uma instanca do model,"
+  # que nesse caso é igual a "quotes/quote, então também podemos remove-lo"
+
+  # a opção "locals" recebe por padrão um valor igual a "{ model_name.element.to_sym => self }",
+  # que no contexto do model Quote é igual a { quote: self }, então também podemos remove-lo
+
+  after_create_commit -> { broadcast_prepend_to 'quotes' }
 end
