f606cbba82a21bf3c2eb7a2bb05d08eff4c18be3
Bruno Faboci <brunofaboci@gmail.com>
2022-09-14 20:06:51 -0300

    update callbacks

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -14,7 +14,8 @@ class Quote < ApplicationRecord
   # a opção "locals" recebe por padrão um valor igual a "{ model_name.element.to_sym => self }",
   # que no contexto do model Quote é igual a { quote: self }, então também podemos remove-lo
 
-  after_create_commit -> { broadcast_prepend_to 'quotes' }
-  after_update_commit -> { broadcast_replace_to 'quotes' }
-  after_destroy_commit -> { broadcast_remove_to 'quotes' }
+  # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
+  # after_update_commit -> { broadcast_replace_later_to 'quotes' }
+  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
+  broadcasts_to ->(quote) { 'quotes' }, inserts_by: :prepend
 end
